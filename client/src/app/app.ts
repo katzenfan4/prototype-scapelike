4ec6818b094838780331ba0e570349c1c2f88d34
Aljosha Volmert <jojscur@gmail.com>
2026-05-13 00:07:06 +0200

    feat(client): add angular websocket client scaffold

---
+++ client/src/app/app.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { GameService } from './services/game.service';
+
+const wsUrl = 'ws://localhost:7070/ws/game';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit, OnDestroy {
+  public messages: string[] = [];
+  private sub!: Subscription;
+
+  constructor(public readonly game: GameService) {}
+
+  ngOnInit(): void {
+    this.sub = this.game.messages$.subscribe((msg) => this.messages.push(msg));
+    this.game.connect(wsUrl);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.game.disconnect();
+  }
+}
