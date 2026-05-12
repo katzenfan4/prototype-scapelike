5209f52f767baf84ef2da41615a3315cee82f2d1
Aljosha Volmert <jojscur@gmail.com>
2026-05-13 00:06:53 +0200

    feat(server): add javalin websocket server scaffold

---
+++ server/src/main/java/com/scapelike/Main.java
@@ -0,0 +1,18 @@
+package com.scapelike;
+
+import com.scapelike.db.Database;
+import com.scapelike.server.GameServer;
+import com.scapelike.tick.TickEngine;
+
+public class Main {
+    public static void main(String[] args) {
+        TickEngine tickEngine = new TickEngine();
+        Runtime.getRuntime().addShutdownHook(new Thread(tickEngine::stop));
+
+        Database db = new Database();
+        GameServer server = new GameServer(db);
+
+        tickEngine.start();
+        server.start();
+    }
+}
