b08380a306020d91ec31197551e67d080f75b915
Aljosha Volmert <aljosha.volmert@nexoma.com>
2026-05-13 00:07:06 +0200

    feat(client): add angular websocket client scaffold

---
+++ client/src/app/services/game.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class GameService {
+  private socket: WebSocket | null = null;
+  public readonly messages$ = new Subject<string>();
+  public connected = false;
+
+  public connect(url: string): void {
+    this.socket = new WebSocket(url);
+    this.socket.onopen = () => (this.connected = true);
+    this.socket.onclose = () => (this.connected = false);
+    this.socket.onmessage = (event) => this.messages$.next(event.data as string);
+  }
+
+  public send(msg: string): void {
+    this.socket?.send(msg);
+  }
+
+  public disconnect(): void {
+    this.socket?.close();
+  }
+}
